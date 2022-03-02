e60c9bef0d72b2a16f51488861c667a3e19daa73
jjsk109 <jjsk109@gmail.com>
2022-03-02 23:41:55 +0900

    first commit

---
+++ src/three/Three_Make.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {AdditiveAnimationBlendMode} from '../three.js-r138/src/Three'
+class Three_Make extends Component {
+    render() {
+        return (
+            <div className="App">
+            <AdditiveAnimationBlendMode></AdditiveAnimationBlendMode>
+            </div>
+        );
+    }
+}
+
+export default Three_Make;
