371ed5e0bf61f39ec02259db3e7f7c70ca10f6a9
jjsk109 <jjsk109@gmail.com>
2022-03-04 23:47:33 +0900

    tree.js first stap

--- src/three/Three_Make.jsx
+++ src/three/Three_Make.jsx
@@ -1,13 +1,26 @@
-import React, { Component } from 'react';
-import {AdditiveAnimationBlendMode} from '../three.js-r138/src/Three'
-class Three_Make extends Component {
-    render() {
-        return (
-            <div className="App">
-            <AdditiveAnimationBlendMode></AdditiveAnimationBlendMode>
-            </div>
-        );
-    }
-}
+import React, { Component , useRef } from 'react';
+import { Canvas ,useFrame } from '@react-three/fiber';
+
+function CoinMesh() {
+    const mesh = useRef(null);
+    useFrame(() => (mesh.current.rotation.y = mesh.current.rotation.z += 0.01));
+    return (
+      <mesh ref={mesh} scale={3}>
+        <cylinderBufferGeometry color="red" args={[2, 1, 0.3, 50]} />
+        <meshLambertMaterial attach="material" />
+      </mesh>
+    );
+  }
+
+const Three_Make = () => {
+    return(
+    <div className="App">
+     <Canvas>
+      <CoinMesh/>
+     </Canvas>
+    </div>
+  );
+};
+
 
 export default Three_Make;
